03390ec9437b93fc80c76156a856f952344f5794
Suraj Boniwal <30743551+suraj1335@users.noreply.github.com>
2020-11-23 23:19:48 +0530

    Search screen done

--- lib/pages/search/search.dart
+++ lib/pages/search/search.dart
@@ -12,26 +12,33 @@ class SearchPage extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               SearchBox(),
-              StaggeredGridView.countBuilder(
-                physics: NeverScrollableScrollPhysics(),
-                shrinkWrap: true,
-                crossAxisCount: 4,
-                itemCount: 10,
-                itemBuilder: (BuildContext context, int index) => Container(
-                  child: Container(
-                    padding: EdgeInsets.all(8),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.all(Radius.circular(12)),
-                      child: Image.network(
-                        'https://picsum.photos/id/${index + 10}/1080/1080',
-                        fit: BoxFit.cover,
+              Padding(
+                padding: EdgeInsets.all(6),
+                child: StaggeredGridView.countBuilder(
+                  physics: NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  crossAxisCount: 6,
+                  itemCount: 20,
+                  itemBuilder: (BuildContext context, int index) {
+                    double borderRadius = index == 0 || index == 1 ? 20 : 0;
+                    return Container(
+                      padding: EdgeInsets.all(2),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(index == 1 ? 0 : borderRadius),
+                          topRight: Radius.circular(index == 0 ? 0 : borderRadius),
+                        ),
+                        child: Image.network(
+                          'https://picsum.photos/id/${index + 50}/1080/1080',
+                          fit: BoxFit.cover,
+                        ),
                       ),
-                    ),
-                  ),
+                    );
+                  },
+                  staggeredTileBuilder: (int index) {
+                    return index == 0 ? StaggeredTile.count(4, 4) : StaggeredTile.count(2, 2);
+                  },
                 ),
-                staggeredTileBuilder: (int index) {
-                  return index == 0 ? StaggeredTile.count(4, 4) : StaggeredTile.count(2, 2);
-                },
               ),
             ],
           ),
