0cc8f7a879f3dc01402e2a5be20d710bab9d8b68
Suraj Boniwal <30743551+suraj1335@users.noreply.github.com>
2020-11-24 00:09:50 +0530

    Changed bottom nav icons

--- lib/pages/profile/profile.dart
+++ lib/pages/profile/profile.dart
@@ -1,9 +1,11 @@
+import 'package:codestagram/styles/colors.dart';
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: ProfileAppBar(padding: MediaQuery.of(context).padding.top),
       body: Container(
         child: Center(
           child: Text('profile'),
@@ -11,4 +13,40 @@ class ProfilePage extends StatelessWidget {
       ),
     );
   }
+}
+
+class ProfileAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final double padding;
+
+  const ProfileAppBar({
+    Key key,
+    this.padding,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return PreferredSize(
+      preferredSize: preferredSize,
+      child: Container(
+        height: double.infinity,
+        padding: EdgeInsets.only(top: padding, left: 12, right: 12),
+        child: Row(
+          children: [
+            Text(
+              'Suraj Boniwal',
+              style: TextStyle(
+                color: AppColors.black,
+                fontFamily: 'Avenir',
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(55);
 }
