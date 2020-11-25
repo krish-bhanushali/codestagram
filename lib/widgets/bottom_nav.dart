9bc47b6f0fa3196d94cb957084b693bd2c88767b
Suraj Boniwal <surajboniwal18@gmail.com>
2020-11-25 12:04:09 +0530

    Resolved bottom nav icons spacing

--- lib/widgets/bottom_nav.dart
+++ lib/widgets/bottom_nav.dart
@@ -77,22 +77,23 @@ class BottomNav extends StatelessWidget {
                         width: ScreenUtil().setWidth(414 / 5),
                         padding: EdgeInsets.symmetric(vertical: 16),
                         child: SvgPicture.asset(
-                          bottomNavController.bottomNavPage.toString() == 'BottomNavPage.search'
-                              ? 'assets/svg/search-f.svg'
-                              : 'assets/svg/search.svg',
+                          bottomNavController.bottomNavPage.toString() == 'BottomNavPage.search' ? 'assets/svg/search-f.svg' : 'assets/svg/search.svg',
                           width: 20,
                           color: AppColors.black,
                         ),
                       ),
                     ),
                     Container(
-                      width: ScreenUtil().setWidth(414 / 10),
-                      height: ScreenUtil().setWidth(414 / 10),
-                      decoration: BoxDecoration(
-                        border: Border.all(color: AppColors.black, width: 1),
-                        borderRadius: BorderRadius.all(Radius.circular(100)),
+                      width: ScreenUtil().setWidth(414 / 5),
+                      height: ScreenUtil().setWidth(414 / 5),
+                      padding: EdgeInsets.all(12),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: AppColors.black, width: 1),
+                          borderRadius: BorderRadius.all(Radius.circular(100)),
+                        ),
+                        child: Icon(Icons.add),
                       ),
-                      child: Icon(Icons.add),
                     ),
                     GestureDetector(
                       onTap: () {
@@ -103,9 +104,7 @@ class BottomNav extends StatelessWidget {
                         width: ScreenUtil().setWidth(414 / 5),
                         padding: EdgeInsets.symmetric(vertical: 16),
                         child: SvgPicture.asset(
-                          bottomNavController.bottomNavPage.toString() == 'BottomNavPage.activity'
-                              ? 'assets/svg/heart-f.svg'
-                              : 'assets/svg/heart.svg',
+                          bottomNavController.bottomNavPage.toString() == 'BottomNavPage.activity' ? 'assets/svg/heart-f.svg' : 'assets/svg/heart.svg',
                           width: 20,
                           color: AppColors.black,
                         ),
